ec977ea76f02d50113969267fb142099cd320472
Tulupov Kirill <kirill.tulupov@mail.ru>
2025-12-12 03:15:56 +0300

    добавил .env - секретки, и примеры. В main добавил импорты из секретки

--- main.py
+++ main.py
@@ -2,26 +2,27 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 from aiogram import Bot, Dispatcher
-import os
-
-from is_admin_data import admins_id_list
-from bot_token import BOT_TOKEN
+import os, dotenv, json
+import environs
 
 
+env = environs.Env()
+env.read_env()
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=env('BOT_TOKEN'))
 dp = Dispatcher()
 
 #фильтр на админа
 class IsAdmin(BaseFilter):
-    def __init__(self, admins_id_list : list[int]) -> None:
-        self.admins_id_list = admins_id_list
+    def __init__(self, admin_id : int) -> None:
+        self.admin_id = admin_id
 
     async def __call__(self, message: Message) -> bool:
-        return message.from_user.id in self.admins_id_list
+        return message.from_user.id is self.admin_id
+    
 
 # Этот хэндлер будет срабатывать, если апдейт от админа
-@dp.message(IsAdmin(admins_id_list))
+@dp.message(IsAdmin(env.int("ADMIN_ID")))
 async def answer_if_admins_update(message: Message):
     await message.answer(text='Вы админ')
 
@@ -31,4 +32,5 @@ async def answer_if_admins_update(message: Message):
 async def answer_if_not_admins_update(message: Message):
     await message.answer(text='Вы не админ')
 
-
+print(os.getenv('BOT_TOKEN'))
+print(os.getenv('ADMIN_ID'))
