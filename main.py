e9357a1d27d2980da711d1c1cd466c7f8d84a96a
Tulupov Kirill <kirill.tulupov@mail.ru>
2025-12-15 23:35:33 +0300

    убрал лишнее

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-
-from aiogram.filters import BaseFilter
 from aiogram.types import Message
 from aiogram import Bot, Dispatcher
 from config.config import load_config
-import json
+from log.logger_config import setup_logger
+from filters.is_admin import IsAdmin
 
 
 config = load_config(".env")
 
+logger = setup_logger('main.py')
+
 bot = Bot(token=config.bot.token)
 dp = Dispatcher()
 dp['admins_id'] = config.bot.admins_id
-
-# Фильтр на админа
-class IsAdmin(BaseFilter):
-    def __init__(self, admins_id : list[int]) -> None:
-        self.admins_id = admins_id
-
-    async def __call__(self, message: Message) -> bool:
-        return message.from_user.id in self.admins_id  # type: ignore
-    
-
-# Хэндлер для админа
-@dp.message(IsAdmin(dp['admins_id']))
-async def answer_if_admins_update(message: Message):
-    await message.answer(text='Вы админ')
 
 
 # Хэндлер для всех остальных
@@ -33,4 +20,5 @@ async def answer_if_not_admins_update(message: Message):
     await message.answer(text='Вы не админ')
 
 if __name__ == "__main__":
+    logger.info("Приложение запускается")
     dp.run_polling(bot)
