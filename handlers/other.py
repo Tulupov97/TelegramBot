fb6283ba3a0421f970fb694247bacf9a5b7f3692
Tulupov Kirill <kirill.tulupov@mail.ru>
2025-12-17 23:41:48 +0300

    хендлер на запрос локаций

--- handlers/other.py
+++ handlers/other.py
@@ -2,7 +2,3 @@ from filters.is_admin import IsAdmin
 from main import dp
 from aiogram.types import Message
 
-# Хэндлер для админа
-@dp.message(IsAdmin(dp['admins_id']))
-async def answer_if_admins_update(message: Message):
-    await message.answer(text='Вы админ')
--- handlers/user.py
+++ handlers/user.py
@@ -1,7 +1,32 @@
-from main import dp
 from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+from aiogram import Router, F
+#from filters import is_admin
+from config.config import load_config
+from keyboards.keybords import keyboard
+from main import bot
 
-# Хэндлер для всех остальных
-@dp.message()
-async def answer_if_not_admins_update(message: Message):
-    await message.answer(text='Вы не админ')
+
+
+user_router = Router()
+
+# Этот хэндлер срабатывает на команду /start
+@user_router.message(CommandStart())
+async def process_start_command(message: Message):
+
+    await message.answer(text=f'Привет {message.from_user.full_name}', reply_markup=keyboard) # type: ignore
+
+@user_router.message(Command(commands="help"))
+async def help_command(message: Message):
+    await message.answer(text=f"/help\n/start")
+
+
+# Этот хэндлер будет срабатывать на ...
+@user_router.message(F.text == 'Локации общих прогулок')
+async def our_location(message: Message):
+    await bot.send_location(
+        chat_id=message.chat.id,
+        latitude=59.867876,
+        longitude=30.329685)
+
+
