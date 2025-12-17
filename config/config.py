698182eeec527c06f27d2588e521802ed861c886
Tulupov Kirill <kirill.tulupov@mail.ru>
2025-12-17 23:41:00 +0300

    ничего

--- config/config.py
+++ config/config.py
@@ -2,19 +2,17 @@ from pydantic import BaseModel
 from environs import Env
 import json
 
-
 class TgBot(BaseModel):
     token : str
     admins_id : list[int]
 
-
 class Config(BaseModel):
     bot : TgBot
-
 
 def load_config(path : str) -> Config:
 
     env = Env()
     env.read_env(override=True)
 
-    return Config(bot = TgBot(token=env("BOT_TOKEN"), admins_id = json.loads(env("ADMINs_ID"))))
+    return Config(bot = TgBot(token=env("BOT_TOKEN"), admins_id = json.loads(env("ADMINS_ID"))))
+
