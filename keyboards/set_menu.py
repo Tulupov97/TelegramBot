3cb1db8285e1c8c240a3811dcd538cf6f4747964
Tulupov <kirill.tulupov@mail.ru>
2025-12-20 15:18:32 +0300

    боковая кнопка меню с командами

---
+++ keyboards/set_menu.py
@@ -0,0 +1,16 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+from lexicon.lexicon import LEXICON_COMMANDS_RU
+
+
+# Функция для настройки кнопки Menu бота
+async def set_default_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(
+            command=command,
+            description=description
+        ) for command, description in LEXICON_COMMANDS_RU.items()
+    ]
+    await bot.set_my_commands(main_menu_commands, scope=BotCommandScopeDefault())
+
