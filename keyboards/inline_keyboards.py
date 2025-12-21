2a6f31805b43e7686b6836d5155a4d01af405b84
Tulupov <kirill.tulupov@mail.ru>
2025-12-21 23:44:43 +0300

    добавил инлайн кнопки

---
+++ keyboards/inline_keyboards.py
@@ -0,0 +1,8 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+lena_group_btn = InlineKeyboardButton(text='Группа "Ленка в меде"', url='https://t.me/chernositovalenaa')
+location_btn = InlineKeyboardButton(text='Локации совместных прогулок', callback_data='Локации совместных прогулок')
+
+
+base_keyboard = InlineKeyboardMarkup(inline_keyboard=[[location_btn]])
+inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[[lena_group_btn]])
