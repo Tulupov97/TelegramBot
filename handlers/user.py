7f5873f1c94cea4acc03c2c13cf69b2e17ac5813
Tulupov Kirill <kirill.tulupov@mail.ru>
2025-12-22 06:17:00 +0300

    добавил ссылку на яндекс карту

--- handlers/user.py
+++ handlers/user.py
@@ -1,33 +1,12 @@
 from aiogram.types import Message, CallbackQuery
-from aiogram.filters import CommandStart, Command
 from aiogram import Router, F
 from config.config import load_config
-from keyboards.keybords import keyboard, admins_keyboard
-from keyboards.inline_keyboards import base_keyboard
-from main import bot
+from keyboards.inline_keyboards import test_keyboard, inline_keyboard
 from log.logger_config import setup_logger
 
 logger = setup_logger("user.py")
 
-
-
 user_router = Router()
-
-# Этот хэндлер срабатывает на команду /start
-@user_router.message(CommandStart())
-async def process_start_command(message: Message):
-    if message.from_user.id not in load_config('.env').bot.admins_id: #type: ignore
-        await message.answer(text=f'Привет {message.from_user.full_name}', reply_markup=keyboard) # type: ignore
-    else:
-        await message.answer(text=f'Привет {message.from_user.full_name}', reply_markup=admins_keyboard) # type: ignore
-
-@user_router.message(Command(commands="help"))
-async def help_command(message: Message):
-    await message.answer(text=f"None")
-
-@user_router.message(Command(commands="other"))
-async def other_command(message: Message):
-    await message.answer(text=f"Группу моей девушки😆", reply_markup=base_keyboard)
 
 
 # Этот хэндлер будет срабатывать на ...
@@ -36,5 +15,12 @@ async def our_location(callback: CallbackQuery):
     await callback.bot.send_location( # type: ignore
         chat_id=callback.message.chat.id, # type: ignore
         latitude=59.867876,
-        longitude=30.329685) 
+        longitude=30.329685)
+    yandex_maps_url = f"https://yandex.ru/maps/?ll={30.329685},{59.867876}&z=17&pt={30.329685},{59.867876}&l=nkarusk"
+    await callback.bot.send_message(
+    chat_id=callback.message.chat.id,
+    text=f"📍 Открыть на Яндекс.Картах: {yandex_maps_url}",
+    disable_web_page_preview=False  # чтобы превью отобразилось
+    )
+
     await callback.answer()
