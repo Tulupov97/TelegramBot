e58701f2d48790aaf34e043566caca8518b0bd50
Tulupov Kirill <kirill.tulupov@mail.ru>
2025-12-22 06:19:31 +0300

    добавил логеры

--- config/config.py
+++ config/config.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
-from environs import Env
+from environs import Env, EnvError
+import logging
+
+logger = logging.getLogger(__name__)
 
 class TgBot(BaseModel):
     token : str
@@ -9,11 +12,21 @@ class Config(BaseModel):
     bot : TgBot
 
 def load_config(path: str | None = None) -> Config:
+    try:
+        env = Env()
+        env.read_env(override=True)
 
-    env = Env()
-    env.read_env(override=True)
-
-    return Config(
-    bot=TgBot(
-        token=env.str("BOT_TOKEN"),
-        admins_id=env.list("ADMINS_ID", subcast=int)))
+        return Config(
+        bot=TgBot(
+            token=env.str("BOT_TOKEN"),
+            admins_id=env.list("ADMINS_ID", subcast=int)))
+    
+    except FileNotFoundError:
+        logger.critical("Конфигурационный файл .env не найден. Проверьте его наличие или переменные окружения.🛑")
+        raise
+    except EnvError as e:
+        logger.critical(f"Ошибка парсинга переменных окружения: {e}. Проверьте корректность .env файла.🛑")
+        raise
+    except Exception as e:
+        logger.critical(f"Неизвестная ошибка при загрузке конфигурации: {e}🛑")
+        raise
