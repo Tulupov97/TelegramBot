c4e35023632dd1325bdadee4a111d6e11e7bce9e
Kirill Tulupov <kirill.tulupov@mail.ru>
2025-12-24 05:32:47 +0300

    добавил инлайн билдер

--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -1,8 +1,27 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from callback_data_factory import CallbackDataFactory
 
-lena_group_btn = InlineKeyboardButton(text='Группа "Ленка в меде"', url='https://t.me/chernositovalenaa')
+
+#клава без билдера
 location_btn = InlineKeyboardButton(text='Локации совместных прогулок', callback_data='Локации совместных прогулок')
-
-
-test_keyboard = InlineKeyboardMarkup(inline_keyboard=[[lena_group_btn]])
 inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[[location_btn]])
+
+
+
+#билдер и кнопки
+builder = InlineKeyboardBuilder()
+
+builder.button(text= 'Группа "Ленка в меде', url='https://t.me/chernositovalenaa')
+builder.button(text= 'Питер', callback_data=CallbackDataFactory(
+        country='Russia',
+        region='Leningrad region',
+        city='Saint-Petersburg'))
+builder.button(text="Москва",
+    callback_data=CallbackDataFactory(
+        country='Russia',
+        region='Moscow oblast',
+        city='Moscow'))
+builder.adjust(1)
+
+
